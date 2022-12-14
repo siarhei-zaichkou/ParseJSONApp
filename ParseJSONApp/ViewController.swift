4c7257a979f5696c933049f16024d332cdfbab6b
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2022-12-14 21:34:37 +0100

    Initial Commit

---
+++ ParseJSONApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParseJSONApp
+//
+//  Created by Sergey on 14.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
